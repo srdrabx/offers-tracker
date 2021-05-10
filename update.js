dad1db0705d97bb13ce07d7931af9fb2f2a9016e
srdrabx <sr.drabx@gmail.com>
2021-05-10 17:25:45 +0200

    Update update.js

--- update.js
+++ update.js
@@ -85,7 +85,7 @@ class Main {
           }
         }
         titles[offer.id] = offer.title;
-        (offer.tags || []).filter(tag => tag).forEach(tag => tags.push(id = tag.id));
+        (offer.tags || []).filter(tag => tag).forEach(tag => tags.push("id": tag.id));
         const thumbnailImage = Array.isArray(offer.keyImages) && offer.keyImages.find(img => img.type === 'Thumbnail');
         list.push([
           offer.id,
